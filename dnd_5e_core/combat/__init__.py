94d646a7212691d39fd9303391dca4f1bebbf998
Philippe Mourey <philippe.mourey@gmail.com>
2026-01-02 01:58:56 +0100

    add combat system

--- dnd_5e_core/combat/__init__.py
+++ dnd_5e_core/combat/__init__.py
@@ -7,11 +7,13 @@ from .damage import Damage
 from .condition import Condition
 from .action import ActionType, Action
 from .special_ability import AreaOfEffect, SpecialAbility
+from .combat_system import CombatSystem, execute_combat_turn
 
 __all__ = [
     'Damage',
     'Condition',
     'ActionType', 'Action',
-    'AreaOfEffect', 'SpecialAbility'
+    'AreaOfEffect', 'SpecialAbility',
+    'CombatSystem', 'execute_combat_turn'
 ]
 
